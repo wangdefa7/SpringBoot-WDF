d8c817c614abb9429dfaed9b095d08216c27124a
WDF <1528401054@qq.com>
2020-05-21 15:29:44 +0800

    打印日志的操作

---
+++ src/main/java/com/wdf/test/javabasic/log/LogBasic.java
@@ -0,0 +1,28 @@
+package com.wdf.test.javabasic.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @Package: com.wdf.test.javabasic.log
+ * @ClassName: LogBasic
+ * @Description: 日志基本操作类
+ * @author: WDF
+ * @date: 2020年5月21日 下午3:21:09
+ * @version: V1.0
+ */
+public class LogBasic {
+	
+	private static Logger logger = LoggerFactory.getLogger(LogBasic.class);
+	
+	public static void main(String[] args) {
+		String msg = "Hello World";
+		logger.info("[{}]",msg);
+		logger.error("[{}]",msg);
+		logger.debug("[{}]",msg);
+		logger.warn("[{}]",msg);
+		System.out.println("-------------");
+		logger.info("[{}]  [{}]",msg,msg);
+	}
+}
---
+++ src/main/java/com/wdf/test/javabasic/log/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * @Package: com.wdf.test.javabasic.log
+ * @ClassName: package-info
+ * @Description: 
+ * @author: WDF
+ * @date: 2020年5月21日 下午3:20:30
+ * @version: V1.0
+ */
+package com.wdf.test.javabasic.log;
