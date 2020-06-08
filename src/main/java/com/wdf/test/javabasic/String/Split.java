a2b7aa9db3cdae9ab84b27ccc68149192b19392c
wangdefa <1528401054@qq.com>
2020-06-08 10:34:13 +0800

    splitTest

---
+++ src/main/java/com/wdf/test/javabasic/String/Split.java
@@ -0,0 +1,10 @@
+package com.wdf.test.javabasic.String;
+
+public class Split {
+	public static void main(String[] args) {
+		String str ="123- ";
+		String[] strz = str.split("-");
+ 		System.out.println(strz[0]);
+	}
+
+}
---
+++ src/main/java/com/wdf/test/javabasic/TestOut.java
@@ -0,0 +1,8 @@
+package com.wdf.test.javabasic;
+
+public class TestOut {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
