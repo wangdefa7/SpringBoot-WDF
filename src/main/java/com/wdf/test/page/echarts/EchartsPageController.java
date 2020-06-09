e0e8f068b6693c42460965b27fc701e04c3ab543
wangdefa7 <1528401054@qq.com>
2020-06-10 07:14:01 +0800

    echarts

---
+++ src/main/java/com/wdf/test/page/echarts/EchartsPageController.java
@@ -0,0 +1,37 @@
+package com.wdf.test.page.echarts;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Echarts配置
+ */
+@RequestMapping("/echarts")
+@Controller
+public class EchartsPageController {
+
+    Logger logger = LoggerFactory.getLogger(EchartsPageController.class);
+
+    /**
+     * echarts菜单
+     * @return
+     */
+    @RequestMapping("/")
+    public String menu(){
+        logger.info("menu。。。。");
+        return "/echarts/menu";
+    }
+
+    /**
+     * 测试demo1
+     * @return
+     */
+    @RequestMapping("demo1")
+    public String openEchartsDemo(){
+        logger.info("openEchartsDemo....");
+        return "/echarts/demo1";
+    }
+}
