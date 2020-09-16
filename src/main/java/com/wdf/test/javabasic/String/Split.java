ea31ca7a779f951f5b622d4889c9dd0a13cf8ff8
wangdefa7 <1528401054@qq.com>
2020-09-16 16:29:21 +0800

    str

--- src/main/java/com/wdf/test/javabasic/String/Split.java
+++ src/main/java/com/wdf/test/javabasic/String/Split.java
@@ -5,6 +5,11 @@ public class Split {
 		String str ="123- ";
 		String[] strz = str.split("-");
  		System.out.println(strz[0]);
+
+ 		String string = "update URP.UNIONPAY_TRANSFLOW set sfsc =";
+		strz = string.split("set");
+		System.out.println(strz[0]);
+		System.out.println(strz[1]);
 	}
 
 }
