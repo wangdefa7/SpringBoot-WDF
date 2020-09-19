e0994aa89bea508e231239d029381d00d84343ec
wangdefa7 <1528401054@qq.com>
2020-09-19 09:37:37 +0800

    Revert "str"
    This reverts commit ea31ca7a

--- src/main/java/com/wdf/test/javabasic/String/Split.java
+++ src/main/java/com/wdf/test/javabasic/String/Split.java
@@ -5,11 +5,6 @@ public class Split {
 		String str ="123- ";
 		String[] strz = str.split("-");
  		System.out.println(strz[0]);
-
- 		String string = "update URP.UNIONPAY_TRANSFLOW set sfsc =";
-		strz = string.split("set");
-		System.out.println(strz[0]);
-		System.out.println(strz[1]);
 	}
 
 }
