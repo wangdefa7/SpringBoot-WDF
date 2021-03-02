4ca2c6f563552038100216414f3c7584aededc42
wangdefa7 <1528401054@qq.com>
2021-03-02 23:20:05 +0800

    HashMap并发

--- src/main/java/com/wdf/test/javabasic/collection/map/HashMapTest.java
+++ src/main/java/com/wdf/test/javabasic/collection/map/HashMapTest.java
@@ -1,10 +1,18 @@
 package com.wdf.test.javabasic.collection.map;
 
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
+/**
+ *                 当多个线程同时检测到总数量超过门限值的时候就会同时调用resize操作，各自生成新的数组并rehash后赋给该map底层的数组table，
+ *                 结果最终只有最后一个线程生成的新数组被赋给table变量，其他线程的均会丢失。而且当某些线程已经完成赋值而其他线程刚开始的时候，
+ *                 就会用已经被赋值的table作为原始数组，这样也会有问题。
+ */
 public class HashMapTest {
 
-    public static final HashMap<String, String> firstHashMap = new HashMap<String, String>();
+    //public static final HashMap<String, String> firstHashMap = new HashMap<String, String>();
+    public static final ConcurrentHashMap<String,String> firstHashMap = new ConcurrentHashMap<String, String>();
 
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread() {
